59f5965c30b55f3b8a206610051c969987a86bb3
Charly Rousseau <c.rousseau@ucl.ac.uk>
2018-04-11 11:14:12 +0100

    Adds docstrings

--- amap/brain/brain_processor.py
+++ amap/brain/brain_processor.py
@@ -8,6 +8,12 @@ from amap.config.atlas import get_atlas_path, get_atlas_pixel_sizes_from_config
 
 
 def get_atlas_pix_sizes():
+    """
+    Get the dictionary of x, y, z from the after loading it or if the atlas size is default,
+    use the values from the config file
+
+    :return: The dictionary of x, y, z pixel sizes
+    """
     atlas = load_atlas()
     pixel_sizes = atlas.header.get_zooms()
     if pixel_sizes != (0, 0, 0):
@@ -17,12 +23,23 @@ def get_atlas_pix_sizes():
 
 
 def load_atlas():
+    """
+    Load the atlas and return it
+
+    :return: The atlas (nifty image)
+    """
     atlas_path = get_atlas_path()
     atlas = bio.load_nii(atlas_path)
     return atlas
 
 
 class BrainProcessor(object):
+    """
+    A class to do some basic processing to the brains (3D image scans) including
+
+    - Changing the orientation
+    - filtering using despeckle and pseudo flatfield
+    """
     def __init__(self, target_brain_path, output_folder, x_pix_mm, y_pix_mm, z_pix_mm,
                  original_orientation='coronal', load_parallel=False):
         self.target_brain_path = target_brain_path
@@ -41,6 +58,7 @@ class BrainProcessor(object):
 
     def flip(self, axes):
         """
+        Flips the brain along the specified axes.
 
         :param tuple axes: a tuple of 3 booleans indicating which axes to flip or not
         :return:
@@ -50,7 +68,16 @@ class BrainProcessor(object):
                 # print("Flipping axis {}".format('xyz'[axis_idx]))
                 self.target_brain = np.flip(self.target_brain, axis_idx)
 
-    def swap_orientation_from_original_to_atlas(self, atlas_orientation='horizontal', flip_z=False):
+    def swap_orientation_from_original_to_atlas(self, atlas_orientation='horizontal'):
+        """
+        Transposes the orientation of the brain from original orientation (an attribute
+        of the class) to atlas orientation.
+        It uses a table of transpositions for each original orientation and assumes the atlas
+        is oriented in the horizontal plane
+
+        :param str atlas_orientation:
+        :return:
+        """
         if atlas_orientation != 'horizontal':
             raise NotImplementedError('Only supported atlas orientation is horizontal')
         transpositions = {
@@ -61,7 +88,16 @@ class BrainProcessor(object):
         transposition = transpositions[self.original_orientation]
         self.target_brain = np.transpose(self.target_brain, transposition)
 
-    def swap_orientation_from_atlas_to_original(self, atlas_orientation='horizontal', flip_z=False):
+    def swap_orientation_from_atlas_to_original(self, atlas_orientation='horizontal'):
+        """
+        Transposes the orientation of the brain from atlas orientation to original orientation
+        (an attribute of the class).
+        It uses a table of transpositions for each original orientation and assumes the atlas
+        is oriented in the horizontal plane.
+
+        :param str atlas_orientation:
+        :return:
+        """
         if atlas_orientation != 'horizontal':
             raise NotImplementedError('Only supported atlas orientation is horizontal')
         transpositions = {
@@ -73,6 +109,12 @@ class BrainProcessor(object):
         self.target_brain = np.transpose(self.target_brain, transposition)
 
     def filter(self):
+        """
+        Applies a set of filters to the brain to avoid overfitting details in the image during
+        registration.
+
+        :return:
+        """
         self.swap_orientation_from_atlas_to_original()  # process along original z dimension
         self.target_brain = BrainProcessor.filter_for_registration(self.target_brain)
         self.swap_orientation_from_original_to_atlas()  # reset to atlas orientation
@@ -80,9 +122,12 @@ class BrainProcessor(object):
     @staticmethod
     def filter_for_registration(brain):
         """
-        From Alex Brown
+        A static method to filter a 3D image to allow registration (avoids overfitting details
+        in the image) (algorithm from Alex Brown).
+        The filter is composed of a despeckle filter using opening and a pseudo flatfield filter
 
-        :return:
+        :return: The filtered brain
+        :rtype: np.array
         """
         brain = brain.astype(np.float64, copy=False)
         for i in trange(brain.shape[-1], desc='filtering', unit='plane'):  # OPTIMISE: could multiprocess but not slow
@@ -92,6 +137,13 @@ class BrainProcessor(object):
         return brain
 
     def save(self, dest_path):
+        """
+        Save self.target_brain to dest_path as a nifty image.
+        The scale (zooms of the output nifty image) is copied from the atlas brain.
+
+        :param str dest_path:
+        :return:
+        """
         atlas_pix_sizes = get_atlas_pix_sizes()
         bio.to_nii(self.target_brain, dest_path,
                    scale=(atlas_pix_sizes['x'], atlas_pix_sizes['y'], atlas_pix_sizes['z']),
@@ -99,12 +151,30 @@ class BrainProcessor(object):
 
 
 def filter_plane_for_registration(img_plane):
+    """
+    Apply a set of filter to the plane (typically to avoid overfitting details in the image during
+    registration)
+    The filter is composed of a despeckle filter using opening and a pseudo flatfield filter
+
+    :param np.array img_plane: A 2D array to filter
+    :return: The filtered image
+    :rtype: np.array
+    """
     img_plane = despeckle_by_opening(img_plane)
     img_plane = pseudo_flatfield(img_plane)
     return img_plane
 
 
 def pseudo_flatfield(img_plane, sigma=5):
+    """
+    Pseudo flat field filter implementation using a detrending by a heavily gaussian filtered
+    copy of the image.
+
+    :param np.array img_plane: The image to filter
+    :param int sigma: The sigma of the gaussian filter applied to the image used for detrending
+    :return: The pseudo flat field filtered image
+    :rtype: np.array
+    """
     # TODO: check gausian filter mode (one of {‘reflect’, ‘constant’, ‘nearest’, ‘mirror’, ‘wrap’})
     img_plane = img_plane.copy()  # FIXME: check if necessary
     filtered_img = gaussian_filter(img_plane, sigma)
@@ -112,11 +182,26 @@ def pseudo_flatfield(img_plane, sigma=5):
 
 
 def scale_to_16_bits(img):
+    """
+    Normalise the input image to the full 0-2^16 bit depth.
+
+    :param np.array img: The input image
+    :return: The normalised image
+    :rtype: np.array
+    """
     normalised = img / img.max()
     return normalised * (2**16 - 1)
 
 
 def despeckle_by_opening(img_plane, radius=2):  # WARNING: inplace operation
+    """
+    Despeckle the image plane using a grayscale opening operation
+
+    :param np.array img_plane:
+    :param int radius: The radius of the opening kernel
+    :return: The despeckled image
+    :rtype: np.array
+    """
     kernel = morphology.disk(radius)
     morphology.opening(img_plane, out=img_plane, selem=kernel)
     return img_plane
